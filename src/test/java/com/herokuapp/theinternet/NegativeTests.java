4da6cac6431cc0eac6b797178509f419670be160
MayaZand <mayaz96101@gmail.com>
2023-12-18 20:06:54 +0200

    Build&run TestNG test suite

--- src/test/java/com/herokuapp/theinternet/NegativeTests.java
+++ src/test/java/com/herokuapp/theinternet/NegativeTests.java
@@ -49,4 +49,48 @@ public class NegativeTests {
         //close browser
         driver.close();
     }
+
+    @Test
+    public void incorrectPasswordTest() {
+        System.out.println("incorrectPasswordTest started");
+        //Create driver
+        WebDriver driver = new ChromeDriver();  //create chrome driver instance.
+        System.out.println("Browser started");
+
+        //open test page
+        String url = "https://the-internet.herokuapp.com/login"; //create new variable to store our url.
+        driver.get(url); //we use "driver" to ope our url.
+
+        driver.manage().window().maximize(); //open driver in full screen.
+        System.out.println("page is opened");
+
+        //enter username
+        WebElement username = driver.findElement(By.id("username"));
+        username.sendKeys("tomsmith"); //enter incorrect username (suppose to be "tomsmith").
+
+        //enter password
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys(("incorrectPassword!"));
+        System.out.println("password is: SuperSecretPassword");
+
+        //click login button
+        WebElement logInButton = driver.findElement(By.tagName("button"));
+        logInButton.click();
+        System.out.println("clicked login button");
+
+        //VERIFICATIONS:
+        WebElement errorMessage = driver.findElement(By.id("flash"));
+        String expectedErrorMessage = "Your password is invalid!";
+        String actualErrorMessage = errorMessage.getText();
+
+        /*to make sure our error message is correct, we will compare our expected to the actual error msg.*/
+        Assert.assertTrue(actualErrorMessage.contains(expectedErrorMessage),
+                "Actual error message does not contain expected. \nActual: "
+                        + actualErrorMessage + "\nExpected: " + expectedErrorMessage);
+
+        //close browser
+        driver.close();
+    }
+
+
 }
